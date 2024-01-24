a6685449913dc68e2f68d1bb4fd3607bd3b32d3e
Timothy Hodson <34148978+thodson-usgs@users.noreply.github.com>
2024-01-24 15:12:43 -0600

    Update recipe.py

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,24 +1,8 @@
 import apache_beam as beam
 import pandas as pd
 
-from numcodecs.abc import Codec
-from pcodec import auto_compress, auto_decompress
-
 from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, StoreToZarr
-
-
-# This codec should be added to numcodecs
-class Pcodec(Codec):
-    def __init__(self):
-        pass
-        
-    def encode(self, buf):
-        return auto_compress(buf)
-
-    def decode(self, buf):
-        return auto_decompress(buf)
-
 
 dates = [
     d.to_pydatetime().strftime('%Y%m%d')
@@ -32,10 +16,10 @@ def make_url(time):
 
 concat_dim = ConcatDim("time", dates, nitems_per_file=1)
 pattern = FilePattern(make_url, concat_dim)
-#encoding={"time": {"compressor": Pcodec()}}
+
 import zarr
 compressor = zarr.Blosc(cname="zstd", clevel=3, shuffle=2)
-encoding={"time": {"compressor": compressor}}
+encoding = {"time": {"compressor": compressor}}
 
 recipe = (
     beam.Create(pattern.items())
